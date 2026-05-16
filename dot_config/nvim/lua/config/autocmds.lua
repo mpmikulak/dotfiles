d055075db8aa375a88eb6a895f31c63ac9070350
Mike Mikulak <mpmikulak@gmail.com>
2026-05-15 21:38:04 -0500

    Adds an alias

---
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,11 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+++ dot_config/nvim/lua/plugins/flutter-tools.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-flutter/flutter-tools.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim", -- Optional: UI enhancements
+  },
+  config = function()
+    require("flutter-tools").setup({
+      widget_guides = { enabled = true },
+      closing_tags = {
+        highlight = "Comment", -- highlight for closing tag
+        prefix = "*** ", -- character to use for close tag e.g. > //
+        enabled = true,
+      },
+      dev_log = {
+        enabled = true,
+        open_cmd = "tabedit", -- or "vsplit"
+      },
+      lsp = {
+        color = { enabled = true },
+      },
+    })
+  end,
+}
